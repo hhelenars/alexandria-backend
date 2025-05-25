d717ff3af6489e6c5a85130fec7f2050e29e0a8d
hhelenars <helenarodriguezsoria@gmail.com>
2025-05-25 16:46:26 +0200

    Crear usuarios modificado

--- src/main/java/com/alexandrianuevo/BackendAlexandriaNuevo/controller/UsuarioController.java
+++ src/main/java/com/alexandrianuevo/BackendAlexandriaNuevo/controller/UsuarioController.java
@@ -3,6 +3,8 @@ package com.alexandrianuevo.BackendAlexandriaNuevo.controller;
 import com.alexandrianuevo.BackendAlexandriaNuevo.model.Usuario;
 import com.alexandrianuevo.BackendAlexandriaNuevo.service.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -18,11 +20,15 @@ public class UsuarioController {
     }
 
     @PostMapping("/registrar")
-    public boolean registrar(@RequestParam String primerNombre,
-                             @RequestParam String segundoNombre,
-                             @RequestParam String email,
-                             @RequestParam String contrasena,
-                             @RequestParam String role) {
-        return usuarioService.registrarUsuario(primerNombre, segundoNombre, email, contrasena, role);
+    public Usuario registrar(@RequestBody Usuario usuario) {
+        boolean exito = usuarioService.registrarUsuario(
+                usuario.getPrimerNombre(),
+                usuario.getSegundoNombre(),
+                usuario.getEmail(),
+                usuario.getContrasena(),
+                usuario.getRole()
+        );
+
+        return exito ? usuario : null;
     }
 }
